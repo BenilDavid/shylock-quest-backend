0cdc625aaa261d3dc23659811a98172b6fa7b3b4
BenilDavid <DoodleDavid>
2022-12-24 14:35:33 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-    origin: ["http://localhost:3000", "https://shylock-quest-development.onrender.com", "https://shylock-quest-development.netlify.app/"]
+    origin: ["http://localhost:3000", "https://shylock-quest-development.onrender.com", "https://shylock-quest-development.netlify.app"]
 }));
 app.use("/api/submit-form",formRoutes);
 // routes
