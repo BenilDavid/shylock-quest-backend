417a23583b15b51aa1818050b70fe7ba7e94f1ec
BenilDavid <DoodleDavid>
2022-12-24 12:34:55 +0530

    added exit process

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -5,10 +5,11 @@ mongoose.set("strictQuery", false);
 const connectDB = async () => {
 try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`mongoDB connected: ${connect.connection.host}`)
+    // console.log(`mongoDB connected: ${connect.connection.host}`);
+    console.log("mongodb connected");
 } catch (error) {
     console.log('error', error);
-    // process.exit(1);
+    process.exit(1);
 }
 }
 
