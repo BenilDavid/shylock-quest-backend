7c61bcb5b8a3d340546ce43616c87372bcaa9330
BenilDavid <DoodleDavid>
2023-01-05 18:59:40 +0530

    added alias and twitter mixed type

--- backend/model/formModel.js
+++ backend/model/formModel.js
@@ -2,18 +2,22 @@ const mongoose = require("mongoose");
 
 const formSchema = mongoose.Schema(
     {
-     twitter: {  
-        type: Object,
-        required: true
-    },
-    metamaskId: {  
-        type: String,
-        required: true
-    }, 
-    answer: {
-        type: String,
-        required: true,
-    }
+        twitter: {
+            type: mongoose.Schema.Types.Mixed,
+            required: true
+        },
+        metamaskId: {
+            type: String,
+            required: true
+        },
+        answer: {
+            type: String,
+            required: true,
+        },
+        alias: {
+            type: String,
+            required: true,
+        }
     },
     {
         timestamps: true
