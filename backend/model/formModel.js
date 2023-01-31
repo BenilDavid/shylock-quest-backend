89ddb4d42ff7374606e9894a02a4fb9cbb0d5176
BenilDavid <DoodleDavid>
2023-01-31 19:16:06 +0530

    update chapter count

--- backend/model/formModel.js
+++ backend/model/formModel.js
@@ -11,7 +11,7 @@ const formSchema = mongoose.Schema(
             required: true
         },
         currentChapterCount: {
-            type: Number,
+            type: String,
             required: false
         },
         metamaskId: {
