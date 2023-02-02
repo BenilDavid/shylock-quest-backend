38654a7616cdc35e4544bbc368caf8ed79a45602
BenilDavid <DoodleDavid>
2023-02-02 21:30:23 +0530

    added testing-quest-chapter in cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: ["http://localhost:3000", "https://shylock-quest-development.onrender.com", "https://shylock-quest-development.netlify.app", "https://shylocknft.com", "https://10-chapter-quest.netlify.app"]
+    origin: ["http://localhost:3000", "https://shylock-quest-development.onrender.com", "https://shylock-quest-development.netlify.app", "https://shylocknft.com", "https://10-chapter-quest.netlify.app", "https://testing-quest-chapter.netlify.app"]
 }));
 app.use("/api/submit-form", formRoutes);
 // routes
